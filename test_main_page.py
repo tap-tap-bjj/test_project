8833a73127b4740e70af20dd5a22be27c5247f4c
tap-tap-bjj <transstandart39@gmail.com>
2023-09-08 17:15:59 +0200

    2-step test on Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,13 @@
-import time
-import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()
+    page.go_to_login_page()
+
+# def go_to_login_page(browser):
+#     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+#     login_link.click()
