529bcd7fad947b836673e0406e9dcb9b932a1cbc
tap-tap-bjj <transstandart39@gmail.com>
2023-09-08 17:50:41 +0200

    3-step test should_be_login_link whith assert check

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,12 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 # def go_to_login_page(browser):
 #     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
 #     login_link.click()
